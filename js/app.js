3494843c8859826a97435c6fe4cba363964faa02
Hasan75 <hasanjab14@gmail.com>
2021-08-20 21:49:09 +0600

    added pin

--- js/app.js
+++ js/app.js
@@ -12,3 +12,22 @@ function generatePin() {
     const pinValue = getPin()
     document.getElementById('display-pin').value = pinValue;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calculatorInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calculatorInput.value = ''
+        }
+    }
+    else {
+        const previousNumber = calculatorInput.value
+        const newNumber = previousNumber + number
+        calculatorInput.value = newNumber
+    }
+})
+
+function verifyPin() {
+
+}
